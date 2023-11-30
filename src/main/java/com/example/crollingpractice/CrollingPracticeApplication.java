f2f956536a26ae4c2b0f53e1a43a9d6f79289426
seonghye0n <>
2023-11-30 17:33:23 +0900

    chore : 프로젝트 설정

---
+++ src/main/java/com/example/crollingpractice/CrollingPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.crollingpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrollingPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrollingPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crollingpractice/CrollingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crollingpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrollingPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
